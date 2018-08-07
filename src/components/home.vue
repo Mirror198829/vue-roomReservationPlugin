5db2dd2483e4a9db448b34329de6754655af0140
Mirror198829 <412366756@qq.com>
2018-08-07 15:20:28 +0800

    上传代码

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="resRoomWrap">
-    <h1 class="resRoomTitle">会议室预订</h1>
-    <room-rerservation></room-rerservation>
+  <div class="resRoomWrap">    
+    <room-res-plug></room-res-plug>
   </div>
 </template>
 
 <script>
-import roomRerservation from './roomRerservation.vue'
+import roomResPlug from './roomRerservation.vue'
 export default {
-  name: 'HelloWorld',
+  name: 'homePage',
   components:{
-    roomRerservation
+    roomResPlug
   },
   data () {
     return {
@@ -22,6 +21,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.resRoomWrap{width:1000px;margin:30px auto;background-color: #fff;min-height: 100%;box-sizing: border-box;}
-.resRoomTitle{font-size:16px;font-weight: 400;padding:14px 20px;border-bottom:2px solid #f6f6f6;}
+
 </style>
